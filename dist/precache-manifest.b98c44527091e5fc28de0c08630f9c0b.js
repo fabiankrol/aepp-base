96650f03f2e24e934fc4f255c9f2185c5de1ffde
Fabian Krol <fabian@krol.io>
2022-04-29 22:29:40 +0200

    Add dist

---
+++ dist/precache-manifest.b98c44527091e5fc28de0c08630f9c0b.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae5438330ac1b65c8c97c0e6fae38df",
+    "url": "/.well-known/apple-app-site-association"
+  },
+  {
+    "revision": "a4864b54ba0989f5152cfca9b10acdab",
+    "url": "/.well-known/assetlinks.json"
+  },
+  {
+    "revision": "273d4859783ae5a90c4e",
+    "url": "/css/chunk-9d3e0f9c.284b4a3c.css"
+  },
+  {
+    "revision": "bde1f2690068a21ed2b7",
+    "url": "/css/page-redeem.ee9bf2fb.css"
+  },
+  {
+    "revision": "fa69ac3dccf09251876f",
+    "url": "/css/popup.19f7baea.css"
+  },
+  {
+    "revision": "7f9ed866a5c626a71506",
+    "url": "/css/popup~ui.b19fdd88.css"
+  },
+  {
+    "revision": "6af21e8f3a5a26d94ddd",
+    "url": "/css/qr-codes.ae5eb665.css"
+  },
+  {
+    "revision": "4aa6ac93b0d9e49181fa",
+    "url": "/css/ui-common.9225468f.css"
+  },
+  {
+    "revision": "b96c0834fadf7ab31810",
+    "url": "/css/ui-common~ui-desktop~ui-mobile.89ab6e0f.css"
+  },
+  {
+    "revision": "ef1865138e78429d0e6c",
+    "url": "/css/ui-desktop.4cf60856.css"
+  },
+  {
+    "revision": "b9688876b1e0503d02d6",
+    "url": "/css/ui-desktop~ui-mobile.1958c93f.css"
+  },
+  {
+    "revision": "a45d4dda579bc26789c5",
+    "url": "/css/ui-mobile.d5a08c2c.css"
+  },
+  {
+    "revision": "007bc3169a71dc67878f",
+    "url": "/css/ui.b84c5b63.css"
+  },
+  {
+    "revision": "51f59f0bdbf09c95c5e58083af4bbcb1",
+    "url": "/default.webmanifest"
+  },
+  {
+    "revision": "53473564e8b78be3df263718a1faac62",
+    "url": "/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "bd5c70f6fcf324cd7f23d793fdf464b8",
+    "url": "/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "64e1038d81288a144458c74c05d24499",
+    "url": "/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "1554e330cbea80ffcefa62d17580e800",
+    "url": "/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "341fbd7df81086774c441021b455824d",
+    "url": "/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "1a292ec0ea571ef3ede2fd2d2d4aab3c",
+    "url": "/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "704ed8ed1bce8bc62a32786e9bd8d0b9",
+    "url": "/favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "496c173265a0dab59f5e104c78fa8023",
+    "url": "/img/1f44b.496c1732.png"
+  },
+  {
+    "revision": "034c002317e072ffa4739907923d3b05",
+    "url": "/img/1f481-200d-2642-fe0f.034c0023.png"
+  },
+  {
+    "revision": "89398478a4c6c59ec6baf72433d9a474",
+    "url": "/img/1f4ab.89398478.png"
+  },
+  {
+    "revision": "df4725986dbd8b0753994a50089090c2",
+    "url": "/img/1f4b8.df472598.png"
+  },
+  {
+    "revision": "f3376f5f66d3c7bfef40b7fa23b567e0",
+    "url": "/img/1f4f2.f3376f5f.png"
+  },
+  {
+    "revision": "516240b8941bc4755e9f4084c89be068",
+    "url": "/img/1f511.516240b8.png"
+  },
+  {
+    "revision": "eff43f357aa920e984cbd8d1dc3a04b2",
+    "url": "/img/1f513.eff43f35.png"
+  },
+  {
+    "revision": "241d36924dc6a7437415dd35150b9f5b",
+    "url": "/img/1f570-fe0f.241d3692.png"
+  },
+  {
+    "revision": "5e984ef9f9447c54689e7de0664025ad",
+    "url": "/img/1f91e.5e984ef9.png"
+  },
+  {
+    "revision": "9c019846d037e02bdfd2b012d602be46",
+    "url": "/img/270d-fe0f.9c019846.png"
+  },
+  {
+    "revision": "6e97219daca21e54352d214e87b51345",
+    "url": "/img/account-card.b989f66c.svg"
+  },
+  {
+    "revision": "9d6364d43d9f0ec3d4d3c7204a93b33b",
+    "url": "/img/aepps.12356716.svg"
+  },
+  {
+    "revision": "bb1331ffc039a2af61fcca44d4cb4434",
+    "url": "/img/phones.be68fcfa.svg"
+  },
+  {
+    "revision": "23fddd65c41faa496811a758c1db6a2c",
+    "url": "/img/send.06be84c2.svg"
+  },
+  {
+    "revision": "37deb198f0bf2b67e39235e72ef5657c",
+    "url": "/img/subaccounts.ec457027.svg"
+  },
+  {
+    "revision": "bece8e21aa326eb1ee51c80b36c9c8aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616f2b6bdebd12def4b4e47849be59ad",
+    "url": "/ios.webmanifest"
+  },
+  {
+    "revision": "757b8434dde713ea9b02",
+    "url": "/js/app.7e7e3a52.js"
+  },
+  {
+    "revision": "273d4859783ae5a90c4e",
+    "url": "/js/chunk-9d3e0f9c.0c063169.js"
+  },
+  {
+    "revision": "e2f23331d1671c41d73f",
+    "url": "/js/chunk-vendors.09949a68.js"
+  },
+  {
+    "revision": "06f09b000a9c1cbb8c5d",
+    "url": "/js/locale-cn.e64bfee9.js"
+  },
+  {
+    "revision": "193909cd4c37561f051a",
+    "url": "/js/locale-es.f717e822.js"
+  },
+  {
+    "revision": "082fe4ee01366722bd31",
+    "url": "/js/locale-ru.e48dd410.js"
+  },
+  {
+    "revision": "bde1f2690068a21ed2b7",
+    "url": "/js/page-redeem.0cd037ca.js"
+  },
+  {
+    "revision": "fa69ac3dccf09251876f",
+    "url": "/js/popup.d587eccc.js"
+  },
+  {
+    "revision": "7f9ed866a5c626a71506",
+    "url": "/js/popup~ui.13298dea.js"
+  },
+  {
+    "revision": "6af21e8f3a5a26d94ddd",
+    "url": "/js/qr-codes.40385025.js"
+  },
+  {
+    "revision": "6d1b33eac69057c9fa38",
+    "url": "/js/socket-io.3a376157.js"
+  },
+  {
+    "revision": "4aa6ac93b0d9e49181fa",
+    "url": "/js/ui-common.fa86326a.js"
+  },
+  {
+    "revision": "b96c0834fadf7ab31810",
+    "url": "/js/ui-common~ui-desktop~ui-mobile.1c01e30e.js"
+  },
+  {
+    "revision": "ef1865138e78429d0e6c",
+    "url": "/js/ui-desktop.2c1a1480.js"
+  },
+  {
+    "revision": "b9688876b1e0503d02d6",
+    "url": "/js/ui-desktop~ui-mobile.44489db6.js"
+  },
+  {
+    "revision": "a45d4dda579bc26789c5",
+    "url": "/js/ui-mobile.9ed1e1ec.js"
+  },
+  {
+    "revision": "007bc3169a71dc67878f",
+    "url": "/js/ui.b04d42a2.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
